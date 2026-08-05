4689181c547efc5401d6422baa1b6ebd7c356e41
Mohnish Kanna KS <mohnishkanna3333@gmail.com>
2026-08-05 20:40:55 +0530

    Initial commit

---
+++ Lab_01/script.sh
@@ -0,0 +1,8 @@
+echo "Welcome to Linux Lab"
+echo ""
+echo "Current username: $(whoami)"
+echo "Current working directory: $(pwd)"
+echo "Date: $(date +"%d-%m-%y")"
+echo "Time: $(date +"%T")"
+
+echo "Course Name in env : $(echo $COURSE_NAME)"
